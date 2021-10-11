2897b7f17dc8405c3ac97a76832610a61dd57ef1
Nick Strikos <nstrikos@yahoo.gr>
2021-10-11 11:18:38 +0300

    fix doc

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1453,7 +1453,7 @@ void MainWindow::showShortcuts()
     QString filename = "C:/Program Files/Audiographs/doc/doc.html";
 
     if ( !QFile::exists(filename)) {
-        filename = "C:/Program Files (x86)/Audiographs/doc/help.html";
+        filename = "C:/Program Files (x86)/Audiographs/doc/doc.html";
     }
 
     QUrl url = QUrl::fromLocalFile(filename);
